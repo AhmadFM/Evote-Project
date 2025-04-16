707a381f3393f2db83df53545e435a1111f06984
Fadhil Anwar <fadhilanwar42@gmail.com>
2025-04-16 12:19:22 +0700

    penyesuaian register

--- register.cpp
+++ register.cpp
@@ -11,7 +11,6 @@ void registerUser ()
     string username, password, role;
     bool verificated = false;
 
-<<<<<<< HEAD
     cout << "================== Register ==================\n";
     cout << "Masukkan Username: ";
     cin >> username;
@@ -19,22 +18,11 @@ void registerUser ()
     cin >> role;
     cout << "Masukkan Password: ";
     cin >> password;
-=======
-    while (!verificated) {
-        cout << "================== Register ==================\n";
-        cout << "Masukkan Username: ";
-        cin >> username;
-        cout << "Pilih peran (panitia/timses/pemilih): "; 
-        cin >> role;
-        cout << "Masukkan Password: ";
-        cin >> password;
->>>>>>> 7da67d8bf9ac3aac408b9ec9e059fccb7ee025d0
-
         verificated = verifRegister(username, password, role);
     }
 
     
-}
+
 
 // int main() {
 //     registerUser();
